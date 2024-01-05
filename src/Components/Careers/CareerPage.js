e89d44838d2a953ed9ecdc259c31db9799d61add
Dharamveer M <dharamveer@podiotracker.co.uk>
2024-01-05 11:40:06 +0530

    helmet

--- src/Components/Careers/CareerPage.js
+++ src/Components/Careers/CareerPage.js
@@ -1,23 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./CareerPage.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
-import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
-import BookIcon from "@mui/icons-material/Book";
-import JoinCard from "./JoinCard";
 // import MediaCard from "./MediaCard";
 // import OpenCard from "./OpenCard";
 // import Team from "../../Images/Career/Team.png";
-import { Link } from "react-router-dom";
 
 // import jobDescriptions from "../Careers/Jobs.json";
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 import CareerBannerComponent from "./CareerBannerComponent";
-import WelcomeToiSearchComponent from "./WelcomeToiSearchComponent";
-import MediaComponent from "./MediaComponent";
 import CarouselComponent from "./CarouselComponent";
 import JoinUsComponent from "./JoinUsComponent";
+import MediaComponent from "./MediaComponent";
+import WelcomeToiSearchComponent from "./WelcomeToiSearchComponent";
 
 const CareerPage = () => {
   return (
@@ -29,14 +23,8 @@ const CareerPage = () => {
         <title>Careers</title>
         <link rel="canonical" href="https://isearch.co.in/careers" />
         <meta name="robots" content="index, follow" />
-        <meta
-          name="news_keywords"
-          content="isearch careers, careers, isearch"
-        />
-        <link
-          rel="image_src"
-          href="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png"
-        />
+        <meta name="news_keywords" content="isearch careers, careers, isearch" />
+        <link rel="image_src" href="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png" />
         <meta
           name="description"
           content="Discover opportunities that exceed your expectations. Let&#039;s stay connected for career opportunities in isearch."
@@ -48,35 +36,20 @@ const CareerPage = () => {
           property="og:description"
           content="Discover opportunities that exceed your expectations. Let&#039;s stay connected for career opportunities in isearch."
         />
-        <meta
-          property="og:image"
-          content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png"
-        />
-        <meta
-          property="og:image:url"
-          content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png"
-        />
+        <meta property="og:image" content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png" />
+        <meta property="og:image:url" content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png" />
         <meta
           property="og:image:secure_url"
           content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png"
         />
         <meta property="og:image:type" content="png" />
-        <meta
-          property="og:image:alt"
-          content='Guy searching "career at isearch" with mobile'
-        />
+        <meta property="og:image:alt" content='Guy searching "career at isearch" with mobile' />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Careers at Amdocs" />
-        <meta
-          name="twitter:image"
-          content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png"
-        />
+        <meta name="twitter:image" content="https://isearch.co.in/static/media/Team.315f13dd8946ded0be17.png" />
         <meta name="MobileOptimized" content="width" />
         <meta name="HandheldFriendly" content="true" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />{" "}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />{" "}
       </Helmet>
       {/* --------------------------------------------------------- */}
 
