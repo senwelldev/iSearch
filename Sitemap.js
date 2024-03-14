3614cfae01c5f174c767ea2826caecbae25dd5dc
dhirajcronage <dhiraj.cronage@gmail.com>
2024-03-14 11:24:53 +0530

    Updated files4

--- Sitemap.js
+++ Sitemap.js
@@ -1,64 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Sitemap = () => {
-  const [sitemap, setSitemap] = useState([]);
-
-  useEffect(() => {
-    // Fetch or import sitemap data here
-    const fetchedSitemap = [
-      { loc: "http://isearch.co.in/", changefreq: "daily", priority: 1.0 },
-      {
-        loc: "http://isearch.co.in/aboutus",
-        changefreq: "weekly",
-        priority: 0.8,
-      },
-      {
-        loc: "http://isearch.co.in/services",
-        changefreq: "weekly",
-        priority: 0.8,
-      },
-      { loc: "http://isearch.co.in/blog", changefreq: "daily", priority: 0.6 },
-      {
-        loc: "http://isearch.co.in/blog/blog1",
-        changefreq: "weekly",
-        priority: 0.5,
-      },
-      {
-        loc: "http://isearch.co.in/blog/blog2",
-        changefreq: "weekly",
-        priority: 0.5,
-      },
-      {
-        loc: "http://isearch.co.in/blog/blog3",
-        changefreq: "weekly",
-        priority: 0.5,
-      },
-      {
-        loc: "http://isearch.co.in/career",
-        changefreq: "monthly",
-        priority: 0.7,
-      },
-      {
-        loc: "http://isearch.co.in/contactus",
-        changefreq: "weekly",
-        priority: 0.7,
-      },
-    ];
-
-    setSitemap(fetchedSitemap);
-  }, []);
+  const sitemapUrls = [
+    { loc: "http://isearch.co.in/", changefreq: "daily", priority: 1.0 },
+    {
+      loc: "http://isearch.co.in/aboutus",
+      changefreq: "weekly",
+      priority: 0.8,
+    },
+    {
+      loc: "http://isearch.co.in/services",
+      changefreq: "weekly",
+      priority: 0.8,
+    },
+    { loc: "http://isearch.co.in/blog", changefreq: "daily", priority: 0.6 },
+    {
+      loc: "http://isearch.co.in/blog/blog1",
+      changefreq: "weekly",
+      priority: 0.5,
+    },
+    {
+      loc: "http://isearch.co.in/blog/blog2",
+      changefreq: "weekly",
+      priority: 0.5,
+    },
+    {
+      loc: "http://isearch.co.in/blog/blog3",
+      changefreq: "weekly",
+      priority: 0.5,
+    },
+    {
+      loc: "http://isearch.co.in/career",
+      changefreq: "monthly",
+      priority: 0.7,
+    },
+    {
+      loc: "http://isearch.co.in/contactus",
+      changefreq: "weekly",
+      priority: 0.7,
+    },
+  ];
 
   const generateXML = () => {
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    sitemap.forEach((item) => {
-      xml += "<url>";
-      xml += `<loc>${item.loc}</loc>`;
-      xml += `<changefreq>${item.changefreq}</changefreq>`;
-      xml += `<priority>${item.priority}</priority>`;
-      xml += "</url>";
+    const today = new Date().toISOString().slice(0, 10);
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+    sitemapUrls.forEach(({ loc, changefreq, priority }) => {
+      xml += `
+        <url>
+          <loc>${loc}</loc>
+          <lastmod>${today}</lastmod>
+          <changefreq>${changefreq}</changefreq>
+          <priority>${priority}</priority>
+        </url>`;
     });
-    xml += "</urlset>";
+    xml += `
+      </urlset>`;
     return xml;
   };
 
