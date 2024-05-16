8f501570bcd261b83489530d2aed49d36b989f04
dhirajcronage <myemail@example.com>
2024-05-16 10:37:33 +0530

    Blog Changes

--- src/Components/Blog/Blog1.js
+++ src/Components/Blog/Blog1.js
@@ -46,7 +46,7 @@ const Blog1 = () => {
           content="SEO stands for “search engine optimization.” In simple terms, SEO means the process of improving your website to increase its visibility in Google, Microsoft Bing, and other search engines whenever people search."
         />
 
-        <meta name="author" content="isearch.co.in" />
+        <meta property="author" content="isearch.co.in" />
         {/*Open Graph / Facebook*/}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://isearch.co.in/blog/blog1" />
@@ -59,7 +59,10 @@ const Blog1 = () => {
           content="SEO stands for “search engine optimization.” In simple terms, SEO means the process of improving your website to increase its visibility in Google, Microsoft Bing, and other search engines ......"
         />
 
-        <meta property="og:image" content={blog1} />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/isearch-130324/o/Blog-images%2FBlogImage1.webp?alt=media&token=679823db-4804-4f86-b381-2d8a143a2bd7"
+        />
         <meta property="og:image:width" content="800px" />
         <meta property="og:image:height" content="600px" />
         {/* Twitter */}
