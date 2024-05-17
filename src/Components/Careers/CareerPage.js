95a4e7389bcfd03c6796137b256bce095b4d3a2f
Dharamveer M <dharamveer@podiotracker.co.uk>
2024-05-17 11:39:50 +0530

    Dynamic blog page

--- src/Components/Careers/CareerPage.js
+++ src/Components/Careers/CareerPage.js
@@ -1,33 +1,14 @@
 import React from "react";
-import "./CareerPage.css";
-import Team from "../../Images/Career/Team.webp";
-import { Helmet } from "react-helmet";
 import CareerBannerComponent from "./CareerBannerComponent";
+import "./CareerPage.css";
 import CarouselComponent from "./CarouselComponent";
 import JoinUsComponent from "./JoinUsComponent";
 import MediaComponent from "./MediaComponent";
 import WelcomeToiSearchComponent from "./WelcomeToiSearchComponent";
-import DocumentMeta from "react-document-meta";
-
-const meta = {
-  title: "Join Our Team",
-  description:
-    "Discover opportunities that exceed your expectations. Let&#039;s stay connected for career opportunities in isearch.",
-  canonical: "https://isearch.co.in/",
-  meta: {
-    charset: "utf-8",
-    name: {
-      keywords: "react,meta,document,html,tags",
-    },
-    image: { Team },
-    "og:image": { Team },
-    type: "article",
-  },
-};
 
 const CareerPage = () => {
   return (
-    <DocumentMeta {...meta}>
+    <DocumentMeta>
       <div className="career-page ">
         {/* ------Code for meta tags of this page------ */}
 
