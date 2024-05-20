ef632f019c9887483bd07dec36473de0c9c5c87c
dhirajcronage <myemail@example.com>
2024-05-20 12:21:15 +0530

    Blog

--- src/Components/Blog/Blog1.js
+++ src/Components/Blog/Blog1.js
@@ -8,7 +8,7 @@ const Blog1 = () => {
   //   "https://firebasestorage.googleapis.com/v0/b/isearch-130324/o/Blog-images%2FBlogImage1.webp?alt=media";
 
   useEffect(() => {
-    document.title = "My Blog";
+    document.title = "My Blog 1";
     let descMeta = document.querySelector("meta[property='og:description']");
     debugger;
     descMeta.setAttribute("content", "Blog Desc");
@@ -24,107 +24,151 @@ const Blog1 = () => {
       <div className="blogsContent">
         <p>
           SEO stands for “
-          <a href="https://en.wikipedia.org/wiki/Search_engine_optimization">search engine optimization</a>
-          .” In simple terms, SEO means the process of improving your website to increase its visibility in Google,
-          Microsoft Bing, and other search engines whenever people search for:
+          <a href="https://en.wikipedia.org/wiki/Search_engine_optimization">
+            search engine optimization
+          </a>
+          .” In simple terms, SEO means the process of improving your website to
+          increase its visibility in Google, Microsoft Bing, and other search
+          engines whenever people search for:
         </p>
         <ul>
           <li>Products you sell.</li>
           <li>Services you provide.</li>
-          <li>Information on topics in which you have deep expertise and/or experience.</li>
+          <li>
+            Information on topics in which you have deep expertise and/or
+            experience.
+          </li>
         </ul>
         <p>
-          The better visibility your pages have in search results, the more likely you are to be found and clicked on.
-          Ultimately, the goal of search engine optimization is to help attract website visitors who will become
-          customers, clients or an audience that keeps coming back.
+          The better visibility your pages have in search results, the more
+          likely you are to be found and clicked on. Ultimately, the goal of
+          search engine optimization is to help attract website visitors who
+          will become customers, clients or an audience that keeps coming back.
         </p>
         <br />
         <p>
-          How is SEO different from <a href="https://en.wikipedia.org/wiki/Search_engine_marketing">SEM</a> and{" "}
-          <a href="https://en.wikipedia.org/wiki/Pay-per-click">PPC</a>?
+          How is SEO different from{" "}
+          <a href="https://en.wikipedia.org/wiki/Search_engine_marketing">
+            SEM
+          </a>{" "}
+          and <a href="https://en.wikipedia.org/wiki/Pay-per-click">PPC</a>?
         </p>
         <p>
-          SEM and PPC are two other common terms that you'll read a lot about in Search Engine Land and hear about in
-          the broader search engine marketing community.
+          SEM and PPC are two other common terms that you'll read a lot about in
+          Search Engine Land and hear about in the broader search engine
+          marketing community.
         </p>
         <p>
-          SEM stands for Search Engine Marketing or as it is commonly known, Search Engine Marketing. Search engine
-          marketing is a form of digital marketing. This is a generic term for a combination of SEO and PPC activities
-          aimed at driving traffic from organic search and paid search. Put simply, search engine marketing is the
-          process of gaining traffic and visibility on search engines through paid and unpaid activities.
+          SEM stands for Search Engine Marketing or as it is commonly known,
+          Search Engine Marketing. Search engine marketing is a form of digital
+          marketing. This is a generic term for a combination of SEO and PPC
+          activities aimed at driving traffic from organic search and paid
+          search. Put simply, search engine marketing is the process of gaining
+          traffic and visibility on search engines through paid and unpaid
+          activities.
         </p>
-        <p>So, what is the difference between SEO and SEM? Technically, it's no different.</p>
+        <p>
+          So, what is the difference between SEO and SEM? Technically, it's no
+          different.
+        </p>
         <p>SEO is only half of SEM:</p>
         <ul>
           <li>SEO = Targeting organic search engine traffic. </li>
           <li>SEM = Get paid and organic traffic from search engines. </li>
         </ul>
         <p>
-          Now, this is where it gets a little confusing. Many people today use SEM interchangeably with
-          PPC(Pay-Per-Click). This idea seems to hurt SEO. However, SEO is marketing, just like PPC marketing is.
+          Now, this is where it gets a little confusing. Many people today use
+          SEM interchangeably with PPC(Pay-Per-Click). This idea seems to hurt
+          SEO. However, SEO is marketing, just like PPC marketing is.
         </p>
         <p>
-          Here's the best way to think about SEO and SEM: Imagine that SEM is a coin. SEO is one side of the coin. PPC
-          is on the other side.
+          Here's the best way to think about SEO and SEM: Imagine that SEM is a
+          coin. SEO is one side of the coin. PPC is on the other side.
         </p>
         <p>
-          PPC is short for pay-per-click, a type of digital marketing where advertisers pay each time one of their ads
-          is clicked. Generally, advertisers bid on specific keywords or phrases for which they want their ads to appear
-          in search results. When a user searches for one of these keywords or phrases, the advertiser's ad appears
-          among the top results. If we look again at search engine marketing as a coin, SEO and PPC are two sides of the
-          same coin: SEO is the unpaid side, PPC is the paid side. It is important never to think in terms of “SEO vs.
-          PPC” since they are complementary channels. As mentioned above, the terms SEM and PPC are used interchangeably
-          in the industry. However, this is not the case with Search Engine Land. Every time we mention "SEM" it's
-          because we mean both SEO (organic search) and PPC (paid search).
+          PPC is short for pay-per-click, a type of digital marketing where
+          advertisers pay each time one of their ads is clicked. Generally,
+          advertisers bid on specific keywords or phrases for which they want
+          their ads to appear in search results. When a user searches for one of
+          these keywords or phrases, the advertiser's ad appears among the top
+          results. If we look again at search engine marketing as a coin, SEO
+          and PPC are two sides of the same coin: SEO is the unpaid side, PPC is
+          the paid side. It is important never to think in terms of “SEO vs.
+          PPC” since they are complementary channels. As mentioned above, the
+          terms SEM and PPC are used interchangeably in the industry. However,
+          this is not the case with Search Engine Land. Every time we mention
+          "SEM" it's because we mean both SEO (organic search) and PPC (paid
+          search).
         </p>
         <p>Types of SEOs</p>
         <ul>
-          <li>Technical SEO: Optimizing the technical aspects of a website. </li>
-          <li>Onsite SEO: Optimize website content for users and search engines.</li>
           <li>
-            Offsite SEO: Create brand assets (e.g., people, people, values, vision, tagline, slogan, colors) and do
-            things that ultimately increase brand awareness and recognition (e.g., demonstrate and develop your
-            knowledge, authority and credibility) and generate demand.
+            Technical SEO: Optimizing the technical aspects of a website.{" "}
+          </li>
+          <li>
+            Onsite SEO: Optimize website content for users and search engines.
+          </li>
+          <li>
+            Offsite SEO: Create brand assets (e.g., people, people, values,
+            vision, tagline, slogan, colors) and do things that ultimately
+            increase brand awareness and recognition (e.g., demonstrate and
+            develop your knowledge, authority and credibility) and generate
+            demand.
           </li>
         </ul>
         <ol>
           <li>
-            Technical Optimization-Optimizing the technical elements of a website is crucial and critical to SEO
-            success. It all starts with architecture: building a website that can be searched and indexed by search
-            engines. Gary Illyes, an analyst, once said in a <a href="https://en.wikipedia.org/wiki/Reddit">Reddit</a>{" "}
-            AMA, "MAKE THIS SITE A COLLECTION OPPORTUNITY. “
+            Technical Optimization-Optimizing the technical elements of a
+            website is crucial and critical to SEO success. It all starts with
+            architecture: building a website that can be searched and indexed by
+            search engines. Gary Illyes, an analyst, once said in a{" "}
+            <a href="https://en.wikipedia.org/wiki/Reddit">Reddit</a> AMA, "MAKE
+            THIS SITE A COLLECTION OPPORTUNITY. “
           </li>
         </ol>
         <p>
-          You want to make it easier for search engines to find and access all the content on your pages (text, images,
-          videos). Which technical elements are important here: URL structure, navigation, internal links, etc.
-          Experience is also an integral part of technical optimization. Search engines emphasize the importance of
-          pages that load quickly and provide a good user experience. Things like core web vitals, usability and mobile
-          usability, HTTPS, and avoiding intrusive interstitials are all important for technical SEO.
+          You want to make it easier for search engines to find and access all
+          the content on your pages (text, images, videos). Which technical
+          elements are important here: URL structure, navigation, internal
+          links, etc. Experience is also an integral part of technical
+          optimization. Search engines emphasize the importance of pages that
+          load quickly and provide a good user experience. Things like core web
+          vitals, usability and mobile usability, HTTPS, and avoiding intrusive
+          interstitials are all important for technical SEO.
         </p>
         <ol start={2}>
           <li>
-            On-Page Optimization-With SEO, your content should be optimized for two main audiences: humans and search
-            engines. That means you're optimizing for what your audience sees (what's actually on the page) and what
-            search engines see (the code). The goal is always to publish quality and useful content. You can achieve
-            this by combining your understanding of what your target audience wants and needs with the data and insights
-            provided by Google.
+            On-Page Optimization-With SEO, your content should be optimized for
+            two main audiences: humans and search engines. That means you're
+            optimizing for what your audience sees (what's actually on the page)
+            and what search engines see (the code). The goal is always to
+            publish quality and useful content. You can achieve this by
+            combining your understanding of what your target audience wants and
+            needs with the data and insights provided by Google.
           </li>
         </ol>
         <p>When optimizing content for people, ensure that:</p>
         <ul>
-          <li>Covers relevant topics in which you have experience or knowledge.</li>
+          <li>
+            Covers relevant topics in which you have experience or knowledge.
+          </li>
           <li>Contains keywords people would use to find your content.</li>
           <li>Is it unique or original?</li>
-          <li>is well written and free from grammatical and spelling mistakes.</li>
+          <li>
+            is well written and free from grammatical and spelling mistakes.
+          </li>
           <li>Contains multimedia content (e.g., images, videos).</li>
           <li>is better than your SERP competitors.</li>
           <li>
-            Is Easy to Read – Structured to make shared information easier to understand (take into account subtitles,
-            paragraph length, use of bold/italics, ordered/unordered lists, reading level, etc.).
+            Is Easy to Read – Structured to make shared information easier to
+            understand (take into account subtitles, paragraph length, use of
+            bold/italics, ordered/unordered lists, reading level, etc.).
           </li>
         </ul>
-        <p>For search engines, some of the most important content elements to optimize are:</p>
+        <p>
+          For search engines, some of the most important content elements to
+          optimize are:
+        </p>
         <ul>
           <li>Title </li>
           <li>Meta Description</li>
@@ -135,13 +179,16 @@ const Blog1 = () => {
         </ul>
         <ol start={3}>
           <p>
-            Offsite Optimization- There are several activities that may not be “SEO” per se, but are nonetheless
-            compatible with SEO success and may indirectly contribute to SEO success.
+            Offsite Optimization- There are several activities that may not be
+            “SEO” per se, but are nonetheless compatible with SEO success and
+            may indirectly contribute to SEO success.
           </p>
           <p>
-            Link building (the process of acquiring links to a website) is the activity most commonly associated with
-            off-site SEO. A different number of links to your website from relevant, credible and reputable websites can
-            bring enormous benefits (e.g., rankings, traffic). The quality of the links outweighs their quantity and the
+            Link building (the process of acquiring links to a website) is the
+            activity most commonly associated with off-site SEO. A different
+            number of links to your website from relevant, credible and
+            reputable websites can bring enormous benefits (e.g., rankings,
+            traffic). The quality of the links outweighs their quantity and the
             goal is to have a large number of quality links.
           </p>
           <p>And how do I get these links?</p>
@@ -151,34 +198,45 @@ const Blog1 = () => {
             You understand:
           </p>
           <ul>
-            <li>Brand Building and Brand Marketing: Techniques to Increase Awareness and Reputation.</li>
-            <li>PR (Public Relations): PR techniques to monetize editorial links.</li>
             <li>
-              Content Marketing: Common forms include video creation, e-books, research, podcasting (or guest posting on
-              other podcasts), and guest posting (or guest blogging).
+              Brand Building and Brand Marketing: Techniques to Increase
+              Awareness and Reputation.
             </li>
             <li>
-              Social Media Marketing & Optimization: Take control of your brand across all relevant platforms, fully
-              optimize and share relevant content.
+              PR (Public Relations): PR techniques to monetize editorial links.
             </li>
             <li>
-              List management: Sending, verifying and optimizing information on any platform where information about
-              your company or website can be shared and found by search engines (e.g., directories, review sites,
-              wikis).
+              Content Marketing: Common forms include video creation, e-books,
+              research, podcasting (or guest posting on other podcasts), and
+              guest posting (or guest blogging).
+            </li>
+            <li>
+              Social Media Marketing & Optimization: Take control of your brand
+              across all relevant platforms, fully optimize and share relevant
+              content.
+            </li>
+            <li>
+              List management: Sending, verifying and optimizing information on
+              any platform where information about your company or website can
+              be shared and found by search engines (e.g., directories, review
+              sites, wikis).
             </li>
             <li>Ratings and reviews: research, monitor and respond.</li>
           </ul>
           <p>
-            When you talk about off-site activities, you're generally talking about activities that, from a purely
-            technical point of view, don't have a direct impact on your SEO abilities.
+            When you talk about off-site activities, you're generally talking
+            about activities that, from a purely technical point of view, don't
+            have a direct impact on your SEO abilities.
           </p>
           <br />
           <p>
-            In a digital world governed by search engines, mastering SEO is the key to unlocking online visibility and
-            engagement. By optimizing content, adapting strategies, and staying attuned to algorithm shifts, you
-            position your brand for success. Remember, SEO isn't just about ranking higher, it's about connecting with
-            your audience authentically. Embrace the evolving landscape, and let SEO be your compass toward sustained
-            online impact.
+            In a digital world governed by search engines, mastering SEO is the
+            key to unlocking online visibility and engagement. By optimizing
+            content, adapting strategies, and staying attuned to algorithm
+            shifts, you position your brand for success. Remember, SEO isn't
+            just about ranking higher, it's about connecting with your audience
+            authentically. Embrace the evolving landscape, and let SEO be your
+            compass toward sustained online impact.
           </p>
         </ol>
       </div>
